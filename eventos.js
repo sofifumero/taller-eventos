c1355eb1565f273c48e432c10d4cdd3d840047cb
unknown <sofifumero>
2024-08-13 12:15:10 -0400

    Implementa solución para evitar la propagación

--- eventos.js
+++ eventos.js
@@ -1,6 +1,7 @@
 // Selecciona el div por su ID
 
 let div = document.getElementById('miDiv');
+let boton = document.querySelector('button');
 
 // Aplica el estilo al div
 
@@ -11,4 +12,11 @@ div.style.backgroundColor = 'green';
 
 div.addEventListener('click', function(){
     alert('Hola! Soy el div');
+});
+
+// Manejador de evento para el click del boton
+
+boton.addEventListener('click', function(event) {
+    alert('Hola!');
+    event.stopPropagation(); // Previene la propagación del evento al div
 });
